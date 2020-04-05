5f276e596e5d7738e75d2046ce6dc0d59abe36a5
jrbeverly <jrbeverly@live.com>
2020-04-05 18:37:52 -0400

    Pull in XGameLib into the project

---
+++ lib/Animation.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/// Animation
+///	 The Animation class is designed to store the start and end index of an animation.
+class Animation
+{
+public:
+	/// Initializes a new instance of Animation.
+	///  @start The start index of the animation.
+	///  @end The end index of the animation.
+	Animation(int start, int end)
+	{
+		_start = start;
+		_end = end;
+	}
+
+	/// Gets the starting index.
+	///  @returns The starting index of the animation.
+	int getStart(void)
+	{
+		return _start;
+	}
+
+	/// Gets the ending index.
+	///  @returns The end index of the animation.
+	int getEnd(void)
+	{
+		return _end;
+	}
+
+	/// The animation delay coefficient.
+	static const int ANIMATION_DELAY = 2;
+
+private:
+	int _start;
+	int _end;
+};
